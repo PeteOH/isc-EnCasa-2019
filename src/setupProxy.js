b647732e1b65be8709cb37aeb4dbab1b8cdbf2b2
PeteOH <peteohalloran@gmail.com>
2019-09-19 13:48:01 +1000

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   // app.use(proxy('/api', { target: 'http://localhost:5000' }));
   app.use(
-    proxy(['/logistics', '/fileserver'], { target: 'http://localhost:52776' })
+    proxy(['/logistics', '/fileserver'], { target: 'http://localhost:52773' })
   );
   app.use(proxy('/notif', { target: 'http://localhost:9999' }));
 };
